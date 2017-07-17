00cd1d9a32cc6ca69927a60b72104cbd3fda79d2
Ashafa11 <ashmor4real@yahoo.com>
2017-07-17 02:07:44 +0100

    Add files via upload

---
+++ CreatedUserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD.Models
+{
+   public  class CreatedUserModel
+    {
+
+        public  string  FirstName { get; set; }
+
+        public string   LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+
+        public static string url = "https://accounts.google.com/SignUp?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F";
+
+        public static string LoginUrl = "https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin";
+
+
+    }
+}
